c2bc510244bcbc7c4ad2c45897a180043a613e04
Esosa Ohangbon <esosaohangbon68@gmail.com>
2025-03-05 20:05:03 -0500

    task: Implemented BitWriter and BitReader for writing/reading from binary file

---
+++ src/bit_stream.cpp
@@ -0,0 +1,51 @@
+#include "bit_stream.hpp"
+
+BitWriter::BitWriter(const std::string& file_name) : output_file(file_name, std::ios::binary), buffer(0), bits_used(0) {
+    if(!output_file.is_open()){
+        throw std::runtime_error("Failed to open the output file");
+    }
+}
+
+BitWriter::~BitWriter(){
+    flush();
+    output_file.close();
+}
+
+void BitWriter::write_bit(bool bit){
+    buffer = (buffer << 1) | bit;
+    bits_used++;
+
+    if (bits_used == 8){
+        output_file.put(buffer);
+        buffer = 0;
+        bits_used = 0;
+    }
+}
+
+void BitWriter::flush(){
+    if (bits_used > 0){
+        buffer <<= (8 - bits_used);
+        output_file.put(buffer);
+        buffer = 0;
+        bits_used = 0;
+    }
+}
+
+BitReader::BitReader(const std::string& file_name) : input_file(file_name, std::ios::binary), buffer(0), bits_remaining(0) {
+    if(!input_file.is_open()){
+        throw std::runtime_error("Failed to open the input file");
+    }
+}
+
+bool BitReader::read_bit(){
+    if (bits_remaining == 0){
+        if (!input_file.get(reinterpret_cast<char&>(buffer))){
+            throw std::runtime_error("Failed to read from input file");
+        };
+        bits_remaining = 8;
+    }
+
+    bool bit = (buffer >> (bits_remaining - 1)) & 1;
+    bits_remaining--;
+    return bit;
+}
---
+++ src/bit_stream.hpp
@@ -0,0 +1,31 @@
+#ifndef BIT_STREAM_HPP
+#define BIT_STREAM_HPP
+
+#include <fstream>
+
+class BitWriter{
+    private:
+        std::ofstream output_file;
+        unsigned char buffer;
+        int bits_used;
+
+    public:
+        BitWriter(const std::string& file_name);
+        ~BitWriter();
+        void write_bit(bool bit);
+        void flush();
+};
+
+class BitReader{
+    private:
+        std::ifstream input_file;
+        unsigned char buffer;
+        int bits_remaining;
+
+    public:
+        BitReader(const std::string& file_name);
+        bool read_bit();
+};
+
+
+#endif
